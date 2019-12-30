8cc982caf582df0cbeb1d54f6f9dab4f36168e3c
Justin Trombley <jtrombley96@gmail.com>
2019-12-30 13:30:54 -0500

    User Form

--- goalstracker/src/App.js
+++ goalstracker/src/App.js
@@ -2,14 +2,17 @@ import React from 'react';
 import Welcome from './components/Welcome'
 import './App.css';
 import {Route} from 'react-router-dom'
+import NewUser from './components/NewUser'
+import Home from './components/Home'
 
 
 function App() {
   return (
     <div className="App">
       <Route exact path='/' component={Welcome} />
-      <Route exact path='/newuser' />
+      <Route exact path='/newuser' component={NewUser}/>
       <Route exact path='/users' />
+      <Route exact path='/home' component={Home}/>
     </div>
   );
 }
---
+++ goalstracker/src/components/Home.js
@@ -0,0 +1,13 @@
+import React, {useState} from 'react'
+import {Link} from 'react-router-dom'
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Welcome!</h1>
+            <button>Goals</button>
+            
+        </div>
+    )
+}
+export default Home
