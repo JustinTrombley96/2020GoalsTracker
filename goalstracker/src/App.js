5261f62fa9146b195bbd1cc4c8e52dcb10f0f340
Justin Trombley <jtrombley96@gmail.com>
2019-12-30 15:38:51 -0500

    Revamped Format

--- goalstracker/src/App.js
+++ goalstracker/src/App.js
@@ -1,20 +1,17 @@
 import React from 'react';
-import Welcome from './components/Welcome'
+import Welcome from './components/Welcome';
 import './App.css';
-import {Route} from 'react-router-dom'
-import NewUser from './components/NewUser'
-import Home from './components/Home'
+import { Route, Link } from 'react-router-dom';
+import NewUser from './components/NewUser';
+import Home from './components/Home';
 
-
-function App() {
-  return (
-    <div className="App">
-      <Route exact path='/' component={Welcome} />
-      <Route exact path='/newuser' component={NewUser}/>
-      <Route exact path='/users' />
-      <Route exact path='/home' component={Home}/>
-    </div>
-  );
+function App () {
+	return (
+			<div className='App'>
+				<Route exact path='/' component={Welcome} />
+				<Route exact path='/home' component={Home} />
+			</div>
+	);
 }
 
 export default App;
--- goalstracker/src/components/Home.js
+++ goalstracker/src/components/Home.js
@@ -1,13 +1,17 @@
-import React, {useState} from 'react'
-import {Link} from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 const Home = () => {
-    return (
-        <div>
-            <h1>Welcome!</h1>
-            <button>Goals</button>
-            
-        </div>
-    )
-}
-export default Home
+    const [goals, setGoals] = useState([])
+	return (
+		<div>
+            <h1>Goals</h1>
+            <h2>Create Goal</h2>
+            <form>
+
+            </form>
+		</div>
+	);
+};
+export default Home;
