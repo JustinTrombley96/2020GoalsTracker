84175d53160ad8039f7f2cd84e1431137dd23555
Justin Trombley <jtrombley96@gmail.com>
2020-01-07 16:55:57 -0500

    Login and Register Component Mapped Out

--- goalstracker/src/App.js
+++ goalstracker/src/App.js
@@ -2,14 +2,17 @@ import React from 'react';
 import Welcome from './components/Welcome';
 import './App.css';
 import { Route, Link } from 'react-router-dom';
-import Goal from './components/Goals';
-import GoalForm from './components/GoalForm';
+import Goals from './components/Goals';
+import Register from './components/Register';
+import Login from './components/Login'
 
 function App () {
     	return (
 			<div className='App'>
 				<Route exact path='/' component={Welcome} />
-				<Route exact path='/goals' component={Goal} />
+				<Route exact path='/register' component={Register} />
+				<Route exact path='/login' component={Login} />
+				<Route exact path='/goals' component={Goals} />
 			</div>
 	);
 }
--- goalstracker/src/components/Welcome.js
+++ goalstracker/src/components/Welcome.js
@@ -2,15 +2,17 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Welcome = () => {
-    
 	return (
-        
 		<div>
 			<h1>A Better You</h1>
 			<h2>You've survived to a new decade. It's time to make a new you.</h2>
-			<Link to ='/goals'>
-                <button>Begin</button>
-            </Link>
+
+			<Link to='/Register'>
+				<button>New User</button>
+			</Link>
+			<Link to='/Login'>
+				<button>Login</button>
+			</Link>
 		</div>
 	);
 };
